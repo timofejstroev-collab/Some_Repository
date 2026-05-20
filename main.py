15ff653a7c7bb8f9182915975a96d6f9fd710412
timof <timofejstroev@gmail.com>
2026-05-20 23:46:54 +0300

    realised ruff on uv update main with errors for test

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-def get_data():
-    x=1
-    y=2
-      z=3
-    return x+y+z
+
+
+def get_data(a, b, c):
+    x = 1
+    y = 2
+    z = 3
+    return x + y + z
+
 
 print(get_data(2, 3, 4))
-
-import os
-import json
-import sys
