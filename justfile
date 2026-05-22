68b3e3a3e26400bbe8eebf64ca032f3f3c8d299a
timof <timofejstroev@gmail.com>
2026-05-22 10:04:08 +0300

    update ruff add lint in justfile

--- justfile
+++ justfile
@@ -1,2 +1,12 @@
-fix:
+lint: ruff-lint
+
+ruff-lint:
     uv run ruff check --fix
+
+#---
+
+fmt: ruff-fmt
+
+ruff-fmt:
+    uv run ruff format
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def get_data(a,b,c):
-    x=1
-    y=2
-    z=3
-    return x+y+z
+def get_data(a, b, c):
+    x = 1
+    y = 2
+    z = 3
+    return x + y + z
 
-print(get_data(2,3,4))
+
+print(get_data(2, 3, 4))
