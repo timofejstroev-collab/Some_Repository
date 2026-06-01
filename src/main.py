fdd4fb5689eb42af1950a4b0ea27682540e8fc41
timof <timofejstroev@gmail.com>
2026-06-01 23:19:03 +0300

    adjusted project structure

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
-def get_data(a, b, c):
-    x = 1
-    y = 2
-    z = 3
+def get_data(x: int, y: int, z: int) -> int:
     return x + y + z
 
 
+def greet(name: str) -> str:
+    return "Hello, " + name
+
+
 print(get_data(2, 3, 4))
+
+result = greet('123')
