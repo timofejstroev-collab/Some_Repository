bd2753ccc9ed65bf010a8c048de42e00b14d9675
timof <timofejstroev@gmail.com>
2026-06-09 19:20:49 +0300

    add new variables

--- src/transports/http_fastapi/depends/app_factory.py
+++ src/transports/http_fastapi/depends/app_factory.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.transports.http_fastapi.routers import router
 
-def create_fastapi_app() -> FastAPI:
-    app = FastAPI(title="My API", debug=True)
-    app.include_router(router)
+APP_TITLE = "My API"
+APP_DEBUG = True
 
+def create_fastapi_app() -> FastAPI:
+    app = FastAPI(title=APP_TITLE, debug=APP_DEBUG)
+    app.include_router(router)
     return app
