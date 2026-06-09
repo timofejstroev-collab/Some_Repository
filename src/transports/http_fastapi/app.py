a628be6e81c9a06ef020f75eb8b347a413fccc07
timof <timofejstroev@gmail.com>
2026-06-09 17:07:06 +0300

    app in def

--- src/transports/http_fastapi/app.py
+++ src/transports/http_fastapi/app.py
@@ -1,4 +1,5 @@
 from src.transports.http_fastapi.depends.app_factory import create_fastapi_app
 
-app = create_fastapi_app()
-
+def appp():
+    app = create_fastapi_app()
+    return app
--- src/transports/http_fastapi/depends/app_factory.py
+++ src/transports/http_fastapi/depends/app_factory.py
@@ -3,7 +3,6 @@ from src.transports.http_fastapi.routers import router
 
 def create_fastapi_app() -> FastAPI:
     app = FastAPI(title="My API", debug=True)
-
     app.include_router(router)
 
     return app
