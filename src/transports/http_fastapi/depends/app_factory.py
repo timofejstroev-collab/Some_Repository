333c0cd3fd8493f91579fdbf9fd68ac366b13e74
timof <timofejstroev@gmail.com>
2026-06-09 21:28:36 +0300

    refactor: move app_title, app_debug into the AppSettings

--- src/transports/http_fastapi/depends/app_factory.py
+++ src/transports/http_fastapi/depends/app_factory.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
 from src.transports.http_fastapi.routers import health_router
+from src.utils.app_settings import get_app_settings
 
-APP_TITLE = "My API"
-APP_DEBUG = True
 
 def create_fastapi_app() -> FastAPI:
-    app = FastAPI(title=APP_TITLE, debug=APP_DEBUG)
+    app = FastAPI(title=get_app_settings().app_title, debug=get_app_settings().app_debug)
     app.include_router(health_router)
     return app
